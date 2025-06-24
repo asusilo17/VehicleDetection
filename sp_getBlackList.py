6130413eab1533d46660c597589883624d633958
Agus Susilo <susilo.agus17@gmail.com>
2025-06-25 06:51:24 +0700

    perbaiakn script

--- sp_getBlackList.py
+++ sp_getBlackList.py
@@ -8,11 +8,31 @@ import requests
 from datetime import datetime
 from dotenv import load_dotenv
 import os
-from Model.sqlProcess import f_sql_savePlateIdentification, f_sql_checking_wanprestasi
+from Model.sqlProcess import f_sql_savePlateIdentification, f_sql_checking_wanprestasi, f_sql_checking_wanprestasi_data
+from Model.botProcess import f_bot_sendMessage, f_bot_sendPhoto, f_bot_sendPhoto_2
 
-data = f_sql_checking_wanprestasi('A120YV',2)
+bot_token = os.getenv('bot_token')
+bot_uname = os.getenv('bot_uname')
+bot_name = os.getenv('bot_name')
 
+datas = f_sql_checking_wanprestasi_data('A1206YV',2)
+rtsp_url = 'DataVideo/videoplayback_2.mp4'
 # if data:
-# print(f"data : {data}")
-if data != 'None':
-    print(f"data : {data}")
+# print(f"datas : {datas}")
+if datas:
+    # print(f"datas : {datas}")
+    for data in datas:
+        print(f"data : {data['Collector']}")
+
+        f_bot_sendPhoto_2(
+                            p_bot_token=bot_token,
+                            p_chatid=data['CollectorTelegram'],
+                            p_pic=data['Collector'],
+                            p_debitorname=data['Collector'],
+                            p_photo_cv2="ImgTest/image_20250604224822.jpeg",
+                            p_license_plate='A1206YV',
+                            p_location=data['LocationName'],
+                            p_address=data['LocationAddress'],
+                            p_maps_url=data['Maps'],
+                            p_date="2025-02-03"
+                        )
