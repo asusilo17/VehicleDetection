516ee0b0d7a6f89ab7497de97d9d8caa107f19da
Agus Susilo <susilo.agus17@gmail.com>
2025-06-26 01:08:10 +0700

    berhasil kirim notif ke telegram

--- CCTV05.py
+++ CCTV05.py
@@ -10,16 +10,21 @@ import glob
 from Model.imageProcess import f_image_read, f_image_preprocessing_010, f_save_image, f_imagesingle_read, f_image_box, f_image_box_license, f_image_box_license_2
 from Model.generalProcess import f_remove_special_char
 from Model.ocrProcess import f_ocr_process
-from Model.sqlProcess import f_sql_savePlateIdentification, f_sql_checking_wanprestasi
-
-load_dotenv()
-
+from Model.sqlProcess import f_sql_savePlateIdentification, f_sql_checking_wanprestasi_data
+from Model.botProcess import f_bot_sendPhoto_2
 
 iot_location_id = os.getenv("iot_location_id")
 # Load YOLOv5 model (Pastikan model sudah di-download)
 # model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=True)
 # model = YOLO("weights/best.pt")
 ocr = PaddleOCR(use_angle_cls=True, lang='en')
+
+bot_token = os.getenv('bot_token')
+bot_uname = os.getenv('bot_uname')
+bot_name = os.getenv('bot_name')
+
+today = datetime.today()
+today = today.strftime('%Y-%M-%d')
 
 # YOLO Modeling
 YoloModelVehicle = YOLO("Yolo/yolov10n.pt")  # Model YOLOv8 Nano untuk deteksi kendaraan
@@ -47,13 +52,14 @@ while cap.isOpened():
         if frame_count % skip_frames != 0:
             continue
 
-        #####################################################################
-        # Vehicle Image Processing
-        #####################################################################
+
 
         vehicle_mask = np.zeros(frame.shape[:2], dtype=np.uint8)
         original_frame = frame.copy()
 
+        #####################################################################
+        # Tahap 1. Mendeteksi Object Kendaraan
+        #####################################################################
         vehicle_results = YoloModelVehicle(frame)
 
         for vehicle in vehicle_results:
@@ -65,10 +71,14 @@ while cap.isOpened():
                 if class_id in [2, 3, 5, 7] and confidence > 0.50:
                     # frame = f_image_box(frame, x1, x2, y1, y2, confidence)
 
+
                     vehicle_crop = frame[y1:y2, x1:x2]
                     vh, vw = vehicle_crop.shape[:2]
                     vehicle_crop_resized = cv2.resize(vehicle_crop, (640, 640))
 
+                    #####################################################################
+                    # Tahap 2. Mendeteksi Plat Nomor Kendaraan
+                    #####################################################################
                     license_plate_results = YoloModelLicenseNumber(vehicle_crop_resized)
 
                     for result in license_plate_results:
@@ -95,6 +105,10 @@ while cap.isOpened():
 
                                 license_crop = original_frame[abs_ly1:abs_ly2, abs_lx1:abs_lx2]
 
+
+                                #####################################################################
+                                # Tahap 3. Proses OCR Plat Nomor Kendaraan
+                                #####################################################################
                                 plat_number, plat_conf = f_ocr_process(license_crop)
 
                                 if plat_number:
@@ -104,10 +118,26 @@ while cap.isOpened():
                                                                     p_license_number=plat_number,
                                                                     p_confidence=lp_conf)
                                     
-                                    dataWanprestasi = f_sql_checking_wanprestasi(plat_number, iot_location_id)
-                                    
+                                    dataWanprestasi = f_sql_checking_wanprestasi_data(plat_number, iot_location_id)
 
-                                
+                                    # if dataWanprestasi:
+                                    #     # print(f"datas : {datas}")
+                                    #     for data in dataWanprestasi:
+                                    #         print(f"data : {data['Collector']}")
+
+                                    #         f_bot_sendPhoto_2(
+                                    #                             p_bot_token=bot_token,
+                                    #                             p_chatid=data['CollectorTelegram'],
+                                    #                             p_pic=data['Collector'],
+                                    #                             p_debitorname=data['Collector'],
+                                    #                             p_photo_cv2=frame,
+                                    #                             p_license_plate=data['PlatNumber'],
+                                    #                             p_location=data['LocationName'],
+                                    #                             p_address=data['LocationAddress'],
+                                    #                             p_maps_url=data['Maps'],
+                                    #                             p_date=today
+                                    #                         )
+
 
     ## Resize Frame
     if not paused:
