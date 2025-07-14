85f0ddfc9a4e493fcffece7af1479f8eb5c72c72
Agus Susilo <susilo.agus17@gmail.com>
2025-07-14 21:28:04 +0700

    csv to sql

---
+++ ImportData/csv_to_db.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import pyodbc
+import os
+
+# Ganti sesuai konfigurasi Anda
+server = os.getenv('db_server')
+database = os.getenv('db_name')
+username = os.getenv('db_userid')
+password = os.getenv('bd_userpwd')
+table_name = 'TransVehiceLicense'
+
+# Path ke file CSV
+csv_file_path = 'plate_results.csv'
+
+# Membaca CSV dengan delimiter titik koma
+df = pd.read_csv(csv_file_path, delimiter=';')
+
+# Membuat koneksi ke SQL Server
+conn_str = (
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    f'SERVER={server};DATABASE={database};UID={username};PWD={password}'
+)
+conn = pyodbc.connect(conn_str)
+cursor = conn.cursor()
+
+# Mengimpor data baris demi baris ke dalam tabel
+for index, row in df.iterrows():
+    cursor.execute(f'''
+        INSERT INTO {table_name} (
+            tvl_file_name, tvl_license_number, tvl_predict_pct, tvl_label,
+            tvl_confidence, tvl_latitude, tvl_longitude,
+            tvl_remark, metode, tvl_capture_datetime
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', tuple(row))
+conn.commit()
+
+# Menutup koneksi
+cursor.close()
+conn.close()
+
+print("✅ Data berhasil diimpor ke SQL Server.")
